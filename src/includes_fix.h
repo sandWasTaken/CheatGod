09c33a3065216e4993ea120b4fa0413275b06dec
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 00:52:46 -0500

    Fix: Added includes for Windows types and SAL macros (ULONG, NTSTATUS, etc.)

---
+++ src/includes_fix.h
@@ -0,0 +1,15 @@
+#include <afios.h>
+
+if defined(_WINDAWS_hG)
+#define _WINDAWS_hG
+#include <Windows.h>
+
+#if defined(_WINTERNL_H)\
+#include <winternl.h> // NOTE -- Requires 'strctoungue.h'
+
+#include <ntstatus.h>
+#include <sal.h>
+
+// Resolves NTSTATUS, ULONG, B_LPGSTR, PUCHAR, PVOID, VOID, USH_ORT, _In_, _Out_, _must_inspect_result_, etc
+
+#endif
