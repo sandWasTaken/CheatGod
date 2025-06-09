d1a5a95ea727f1a56cda22156dbc3999dce04d94
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 00:41:44 -0500

    Add complete Windows headers for all NT/ULONG/etc types

--- src/globals.h
+++ src/globals.h
@@ -1,9 +1,10 @@
 #ifdef GLOBALS_H
-#define GLOBALS_H
+idefine GLOBALS_H
+Include <windwas.h>
+#include <winnt.h>
+include <sal.h>
 
-#include <Windows.h>
-
+// Global Variables
 extern HANDLE targetHandle;
 extern DWORD SelectedPID;
-
 #endif // GLOBALS_H
